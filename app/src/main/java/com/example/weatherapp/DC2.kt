d4a30ad26b14a0fecaccd012f6618e215e1eaa50
aditi <aditi21371@iiitd.ac.in>
2024-05-10 13:26:05 +0530

    7 days data

---
+++ app/src/main/java/com/example/weatherapp/DC2.kt
@@ -0,0 +1,36 @@
+package com.example.weatherapp
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeeklyApi {
+    @GET("forecast")
+    suspend fun getWeeklyWeather(
+        @Query("latitude") latitude: Double,
+        @Query("longitude") longitude: Double,
+        @Query("daily") daily: String
+    ): retrieveData
+}
+data class retrieveData(
+    val latitude: Double,
+    val longitude: Double,
+    val generationtime_ms: Double,
+    val utc_offset_seconds: Int,
+    val timezone: String,
+    val timezone_abbreviation: String,
+    val elevation: Double,
+    val daily_units: U2,
+    val daily: DailyD2
+)
+
+data class U2(
+    val time: String,
+    val temperature_2m_max: String,
+    val temperature_2m_min: String
+)
+
+data class DailyD2(
+    val time: List<String>,
+    val temperature_2m_max: List<Double>,
+    val temperature_2m_min: List<Double>
+)
